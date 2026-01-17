a5bbb110b201a33d257a236a32d907c372397a03
Taimur Qazi <qazitaimur9001@gmail.com>
2026-01-17 03:41:23 -0500

    fix 2

--- apps/api/controllers/projects.controller.ts
+++ apps/api/controllers/projects.controller.ts
@@ -27,7 +27,6 @@ class ProjectsController {
                 billingCycleEndDate,
                 imageUrl,
                 vendorIds,
-                invoiceIds
             } = req.body;
 
             if (!name) {
@@ -61,8 +60,7 @@ class ProjectsController {
 
             const project = await projectsServices.createProject(
                 projectData,
-                vendorIds || [],
-                invoiceIds || []
+                vendorIds || []
             );
 
             return res.status(201).json({
